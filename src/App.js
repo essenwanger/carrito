7c2b004cd3c91aaac49a8654387c4533ca45774b
Victor López <vic@Victors-MacBook-Pro.local>
2019-11-16 22:17:44 -0500

    Camera

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import getTheme from './themes/components';
 import ecommerce from './themes/variables/ecommerce';
 import LoginScene from './scenes/LoginScene'
 import MainScene from './scenes/MainScene'
+import CameraScene from './scenes/CameraScene'
 import carrito from './reducers';
 import { createStore } from 'redux';
 
@@ -16,8 +17,9 @@ export default class App extends Component {
       <StyleProvider style={getTheme(ecommerce)}>
       <Router>
         <Stack key="root">
-          <Scene key="login" component={()=><LoginScene store={store} />} hideNavBar initial/>
+          <Scene key="login" component={()=><LoginScene store={store} />} hideNavBar />
           <Scene key="main" component={()=><MainScene store={store} />} hideNavBar />
+          <Scene key="main" component={()=><CameraScene store={store} />} hideNavBar initial/>
         </Stack>
       </Router>
       </StyleProvider>
